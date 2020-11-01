96a3f8d2d0caee3e4adabe7775571b8fd91e7937
Tobias Pitters <tobias.pitters@gmail.com>
2020-11-01 22:50:48 +0100

    add basic structure, add previous tries

---
+++ flask_test/app.py
@@ -0,0 +1,32 @@
+import sqlite3
+from flask import Flask, render_template
+from werkzeug.exceptions import abort
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<int:post_id>')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
