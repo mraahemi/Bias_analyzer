f6191342d76533c55cf691379625cd7a348c28ac
Tobias Pitters <tobias.pitters@gmail.com>
2020-11-05 07:26:00 +0100

    add flask_model_analyzer

--- flask_model_analyzer/app.py
+++ flask_model_analyzer/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 from datetime import datetime
 
 app = Flask(__name__)
@@ -16,7 +17,24 @@ class Todo(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('base.html')
+    conn = get_db_connection()
+    models = conn.execute('SELECT * FROM models').fetchall()
+    conn.close()
+    # todo: add possibility to upload model
+    return render_template('index.html', models=models)
+
+@app.route('/models/<int:id>')
+def show_model(id):
+    conn = get_db_connection()
+    model_overview = conn.execute(f'SELECT * FROM models where id={id}').fetchall()
+    print('this is the model overview')
+    print(model_overview)
+    return render_template('model_overview.html', model_overview=model_overview)
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 if __name__ == "__main__":
     app.run(debug=True, use_debugger=False, use_reloader=False)
